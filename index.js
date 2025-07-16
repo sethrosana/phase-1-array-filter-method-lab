4ce855195f1c4c44ebc3eb47539c5256509a77ba
sethmorara <seth.morara@student.moringaschool.com>
2025-07-16 15:32:05 +0300

    updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(solution, slur) {
+  return solution.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === slur.toLowerCase()
+  );
+}
+
+function fuzzyMatch(source, section) {
+  return source.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(section.toLowerCase()) === 0
+  );
+}
+
+function matchName(source, soughtName) {
+  return source.filter((record) => record.name === soughtName);
+}
